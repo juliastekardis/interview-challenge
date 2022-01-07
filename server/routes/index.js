22514c81954fdf828aff92682351cb07827ea61d
Julia Stekardis <jmst323@uky.edu>
2022-01-07 10:35:38 -0500

    Task 6: modified server/routes/index.js to change Percentage col. to percentage

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { State } = require('../models');
+const sequelize = require('../util/SequelizeManager');
 
 // TODO: Create a GET '/states' route that returns data from your State model
 // Note: The functions to retrieve data from the database using sequelize is asyncronous, so it will look something like this:
 // var people = await Person.doSomething();
 router.get('/states', async (req, res) => { // GET route
-    var states = await State.findAll(); // obtain all data from State model
+    var states = await State.findAll({
+        attributes: {
+            include: [
+                // modify 'percent' column to show as percentage rounded to 2 decimal places
+                [sequelize.literal('CONCAT(ROUND(percent * 100,2),"%")'), 'percent'] 
+            ]
+        }
+        
+    }); // obtain all data from State model
     res.send(states); // send all data
 });
 
