6b5a1289f15b5373166983ceacbf2d0c09d4dde3
Julia Stekardis <jmst323@uky.edu>
2022-01-06 23:32:57 -0500

    Task 4: modified server/routes/index.js to create GET route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,9 @@ const { State } = require('../models');
 // TODO: Create a GET '/states' route that returns data from your State model
 // Note: The functions to retrieve data from the database using sequelize is asyncronous, so it will look something like this:
 // var people = await Person.doSomething();
+router.get('/states', async (req, res) => { // GET route
+    var states = await State.findAll(); // obtain all data from State model
+    res.send(states); // send all data
+});
 
 module.exports = router;
